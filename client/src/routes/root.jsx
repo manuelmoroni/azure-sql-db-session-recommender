b390687a30df14d423b3d76e593d97ab0228a160
Manuel Moroni <manuel.moroni@outlook.com>
2023-12-30 19:22:11 +0100

    home

--- client/src/routes/root.jsx
+++ client/src/routes/root.jsx
@@ -29,6 +29,7 @@ function showSessionCount(sessionsCount) {
 export default function Root() {
   const { userInfo, sessionsCount } = useLoaderData();
 
+  /*
   return (
     <>
       <div id="header">
@@ -46,6 +47,27 @@ export default function Root() {
         </p>
         <p className="font-subtitle-2">
           Source code and and related articles is <a href="https://github.com/Azure-Samples/azure-sql-db-session-recommender">available on GitHub.</a>
+        </p>
+        <React.Suspense fallback={showSessionCount()}>
+          <Await resolve={sessionsCount} errorElement={(<p className="font-subtitle-2">Unable to load session count 😥...</p>)}>
+            {(sessionsCount) => showSessionCount(sessionsCount)}
+          </Await>
+        </React.Suspense>
+      </div>
+      <div>
+        <Outlet />
+      </div>
+    </>
+  );
+  */
+
+  return (
+    <>
+      <div id="header">
+        <h1 className="font-title-1">Cool Conference Session Finder - V.1.0</h1>
+        <p className="font-subtitle-1">
+          Use OpenAI to search for interesting sessions. Write the topic you're interested in, and (up to) the top ten most interesting and related session will be returned.
+          The search is done using <a href="https://learn.microsoft.com/en-us/azure/ai-services/openai/concepts/models#embeddings-models">text embeddings</a> and then using <a href="https://en.wikipedia.org/wiki/Cosine_similarity" target="_blank">cosine similarity</a> to find the most similar sessions.
         </p>
         <React.Suspense fallback={showSessionCount()}>
           <Await resolve={sessionsCount} errorElement={(<p className="font-subtitle-2">Unable to load session count 😥...</p>)}>
